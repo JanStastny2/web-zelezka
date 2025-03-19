9d40d3f30b216392d3c1ce86af892e7271a8f3ff
JanStastny2 <stastnyj162@gmail.com>
2025-03-19 12:07:31 +0100

    Revert "opraveno dle gpt"
    This reverts commit fe4ee5fb48b14f2106ac2d71d60dd7eed1f02096.
    
    	modified:   app.js
    	modified:   fotogalerie.html
    	modified:   index.html
    	modified:   oklubu.html
    	modified:   statistiky.html
    	modified:   style.css
    	modified:   tymy.html

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-
 const btNav = document.getElementById('bt-nav');
-const spanCloseModal = document.getElementById('close-modal');
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const buttons = document.querySelectorAll(".bt-fotogalerie");
+
+  buttons.forEach(button => {
+      button.addEventListener("click", function () {
+        console.log("dobry");
+      });
+  });
+  
+  
+});
+
+
 
 const handleResize = () => {
     const btNav = document.getElementById('bt-nav');
@@ -14,34 +27,6 @@ const handleResize = () => {
       hideElement(nav);
     }
 };
-
-document.addEventListener('DOMContentLoaded', function() {
-    const buttons = document.querySelectorAll('.img-fotogalerie');
-    const modal = document.getElementById('modal');
-
-    buttons.forEach(button => { button.addEventListener('click',function (){
-      //pomoci this zobrazim ten img, na ktery jsem kliknul
-        const img = document.getElementById('modal-image');
-        const modalBackground = document.querySelector('#body-fotogalerie');
-        img.src = this.src;
-        this.classList.add('modal-content');
-        console.log(this);
-        modalBackground.className = "modal-background";
-        
-        showElement(modalBackground);
-        showElement(modal);
-      });
-   });
-});
-
-function closeModal() {
-  const modalBackground = document.querySelector('#body-fotogalerie');
-  const modal = document.getElementById('modal');
-  modalBackground.className = "body-fotogalerie"
-  hideElement(modal);
-
-}
-
 
 function navMouseOver() {
     const item = document.getElementById('bt-nav');
@@ -90,6 +75,10 @@ function navMouseClicked() {
     }
 }
 
+function zoomImage() {
+  alert("ca");
+    const img = document.getElementById('');
+}
 
 window.addEventListener('load',handleResize);
 window.addEventListener('resize', handleResize);
